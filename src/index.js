d7333b83c3b5a9e5684880f8e1330156f228dc4b
Vlada-only-one <vlada.only.one@gmail.com>
2021-03-16 02:27:57 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-module.exports = function reverse(n) {
-	return parseInt(String(n).split('').reverse().join(''));
+module.exports = function reverse(number) {
+	return parseInt(String(number).split('').reverse().join(''));
 }
